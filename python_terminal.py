541386b8419df4c8137394670c25c9799d17c0e2
Raymond Xu <raymond.y.xu@gmail.com>
2020-09-08 21:22:56 -0400

    added default terminal

---
+++ python_terminal.py
@@ -0,0 +1,9 @@
+print('import from ~/tools/python_terminal.py')
+import re, time, os, sys, csv
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import imageio
+from rich import print
+
+print('imported re, time, os, csv, sys, np, plt, math, imageio, rich.print')
